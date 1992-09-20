f6f7bf23ec6d73dd0c8729a856044b0d7e0267cf
Mark R. Devlin <markd@users.sourceforge.net>
1992-09-20 23:30:27 +0000

    Provides commands compatible with older versions of Extended Tcl.

---
+++ tcl/runtime/compat.tcl
@@ -0,0 +1,24 @@
+#
+# compat --
+#
+# This file provides commands compatible with older versions of Extended Tcl.
+# 
+#------------------------------------------------------------------------------
+# Copyright 1992 Karl Lehenbauer and Mark Diekhans.
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted, provided
+# that the above copyright notice appear in all copies.  Karl Lehenbauer and
+# Mark Diekhans make no representations about the suitability of this
+# software for any purpose.  It is provided "as is" without express or
+# implied warranty.
+#------------------------------------------------------------------------------
+# $Id$
+#------------------------------------------------------------------------------
+#
+
+#@package: TclX-Compatibility execvp
+
+proc execvp {progname args} {
+    execl $progname $args
+}
