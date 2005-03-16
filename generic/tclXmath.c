cf15a970e3df5a9c745a771d2aa2098b6e4bf7d4
Jeffrey Hobbs <hobbs@users.sourceforge.net>
2005-03-16 23:48:21 +0000

    * generic/tclXmath.c: add ability to handle wide values in min/max 	math functions and commands. 	Merged TclX_MinFunc and TclX_MaxFunc into TclX_MinMaxFunc.

--- generic/tclXmath.c
+++ generic/tclXmath.c
@@ -11,8 +11,11 @@
  * Mark Diekhans make no representations about the suitability of this
  * software for any purpose.  It is provided "as is" without express or
  * implied warranty.
+ *
+ * Copyright 2005 ActiveState Corporation.
+ *
  *-----------------------------------------------------------------------------
- * $Id: tclXmath.c,v 8.7 1999/03/31 06:37:45 markd Exp $
+ * $Id: tclXmath.c,v 1.1 2001/10/24 23:31:48 hobbs Exp $
  *-----------------------------------------------------------------------------
  */
 
@@ -23,46 +26,43 @@
 #  define tclx_random random
 #endif
 
+#ifdef TCL_WIDE_INT_TYPE
+#define GET_DOUBLE_VALUE(doubleVar, argPtr, type)		\
+    if ((type) == TCL_INT) {					\
+	(doubleVar) = (double) (argPtr).intValue;		\
+    } else if ((type) == TCL_WIDE_INT) {			\
+	(doubleVar) = Tcl_WideAsDouble((argPtr).wideValue);	\
+    } else { /* TCL_DOUBLE */					\
+	(doubleVar) = (argPtr).doubleValue;			\
+    }
+#else
+#define GET_DOUBLE_VALUE(doubleVar, argPtr, type)		\
+    if ((type) == TCL_INT) {					\
+	(doubleVar) = (double) (argPtr).intValue;		\
+    } else { /* TCL_DOUBLE */					\
+	(doubleVar) = (argPtr).doubleValue;			\
+    }
+#endif
+
 /*
  * Prototypes of internal functions.
  */
-static int
-ConvertIntOrDoubleObj _ANSI_ARGS_((Tcl_Interp *interp,
-                                   Tcl_Obj    *numStrObj,
-                                   double     *valuePtr));
+static int	ConvertIntOrDoubleObj (Tcl_Interp *interp,
+				Tcl_Obj *numStrObj, double *valuePtr);
 
-static long 
-ReallyRandom _ANSI_ARGS_((long my_range));
+static long	ReallyRandom (long my_range);
 
-static int
-TclX_MaxObjCmd _ANSI_ARGS_((ClientData  clientData,
-                            Tcl_Interp *interp,
-                            int         objc,
-                            Tcl_Obj   *CONST objv[]));
+static int	TclX_MaxObjCmd (ClientData clientData, Tcl_Interp *interp,
+				int objc, Tcl_Obj *CONST objv[]);
 
-static int
-TclX_MinObjCmd _ANSI_ARGS_((ClientData  clientData,
-                            Tcl_Interp *interp,
-                            int         objc,
-                            Tcl_Obj   *CONST objv[]));
+static int	TclX_MinObjCmd (ClientData clientData, Tcl_Interp *interp,
+				int objc, Tcl_Obj *CONST objv[]);
 
-static int
-TclX_MaxFunc _ANSI_ARGS_((ClientData  clientData,
-                          Tcl_Interp *interp,
-                          Tcl_Value  *args,
-                          Tcl_Value  *resultPtr));
+static int	TclX_MinMaxFunc (ClientData clientData, Tcl_Interp *interp,
+				Tcl_Value *args, Tcl_Value *resultPtr);
 
-static int
-TclX_MinFunc _ANSI_ARGS_((ClientData  clientData,
-                          Tcl_Interp *interp,
-                          Tcl_Value  *args,
-                          Tcl_Value  *resultPtr));
-
-static int
-TclX_RandomObjCmd _ANSI_ARGS_((ClientData  clientData,
-                               Tcl_Interp *interp,
-                               int         objc,
-                               Tcl_Obj   *CONST objv[]));
+static int	TclX_RandomObjCmd (ClientData  clientData, Tcl_Interp *interp,
+				int objc, Tcl_Obj *CONST objv[]);
 
 
 /*-----------------------------------------------------------------------------
@@ -85,12 +85,21 @@ ConvertIntOrDoubleObj (interp, numStrObj, valuePtr)
     Tcl_Obj    *numStrObj;
     double     *valuePtr;
 {
+#ifdef TCL_WIDE_INT_TYPE
+    Tcl_WideInt wVal;
+
+    if (Tcl_GetWideIntFromObj (interp, numStrObj, &wVal) == TCL_OK) {
+	*valuePtr = Tcl_WideAsDouble(wVal);
+	return TCL_OK;
+    }
+#else
     long lvalue;
 
     if (Tcl_GetLongFromObj (interp, numStrObj, &lvalue) == TCL_OK) {
 	*valuePtr = (double) lvalue;
 	return TCL_OK;
     }
+#endif
 
     if (Tcl_GetDoubleFromObj (interp, numStrObj, valuePtr) == TCL_OK)
 	return TCL_OK;
@@ -178,80 +187,61 @@ TclX_MinObjCmd (clientData, interp, objc, objv)
  *-----------------------------------------------------------------------------
  */
 static int
-TclX_MaxFunc (clientData, interp, args, resultPtr)
+TclX_MinMaxFunc (clientData, interp, args, resultPtr)
     ClientData  clientData;
     Tcl_Interp *interp;
     Tcl_Value  *args;
     Tcl_Value  *resultPtr;
 {
-    if ((args [0].type == TCL_INT) && (args [1].type == TCL_INT)) {
-        resultPtr->type = TCL_INT;
+    int isMax = (int) clientData;
+    Tcl_ValueType t0 = args[0].type;
+    Tcl_ValueType t1 = args[1].type;
 
-        if (args [0].intValue > args [1].intValue) {
-            resultPtr->intValue = args [0].intValue;
-        } else {
-            resultPtr->intValue = args [1].intValue;
-        }
-    } else {
-        double values [2];
-
-        resultPtr->type = TCL_DOUBLE;
-
-        values [0] = (args [0].type == TCL_INT) ? (double) args [0].intValue :
-                                                  args [0].doubleValue;
-        values [1] = (args [1].type == TCL_INT) ? (double) args [1].intValue :
-                                                  args [1].doubleValue;
-
-        if (values [0] > values [1]) {
-            resultPtr->doubleValue = values [0];
-        } else {
-            resultPtr->doubleValue = values [1];
-        }
-    }
-    return TCL_OK;
-}
-
-/*-----------------------------------------------------------------------------
- *
- * TclX_MinFunc --
- *      Implements the Tcl min math function
- *        expr min(num1, num2)
- *
- * Results:
- *      Standard TCL results.
- *
- *-----------------------------------------------------------------------------
- */
-static int
-TclX_MinFunc (clientData, interp, args, resultPtr)
-    ClientData  clientData;
-    Tcl_Interp *interp;
-    Tcl_Value  *args;
-    Tcl_Value  *resultPtr;
-{
-    if ((args [0].type == TCL_INT) && (args [1].type == TCL_INT)) {
-        resultPtr->type = TCL_INT;
-
-        if (args [0].intValue < args [1].intValue) {
-            resultPtr->intValue = args [0].intValue;
-        } else {
-            resultPtr->intValue = args [1].intValue;
-        }
-    } else {
-        double values [2];
+    if ((t1 == TCL_DOUBLE) || (t0 == TCL_DOUBLE)) {
+	double d0, d1;
+	/*
+	 * Compare as doubles.
+	 */
+	GET_DOUBLE_VALUE(d0, args[0], t0);
+	GET_DOUBLE_VALUE(d1, args[1], t1);
 
         resultPtr->type = TCL_DOUBLE;
+	if (isMax) {
+	    resultPtr->doubleValue = (d0 < d1) ? d1 : d0;
+	} else {
+	    resultPtr->doubleValue = (d0 > d1) ? d1 : d0;
+	}
+#ifdef TCL_WIDE_INT_TYPE
+    } else if ((t1 == TCL_WIDE_INT) || (t0 == TCL_WIDE_INT)) {
+	Tcl_WideInt w0, w1;
+	/*
+	 * Compare as wide ints (neither are doubles)
+	 */
+	w0 = (t0 == TCL_INT) ? Tcl_LongAsWide(args[0].intValue) :
+	    args[0].wideValue;
+	w1 = (t1 == TCL_INT) ? Tcl_LongAsWide(args[1].intValue) :
+	    args[1].wideValue;
 
-        values [0] = (args [0].type == TCL_INT) ? (double) args [0].intValue :
-                                                  args [0].doubleValue;
-        values [1] = (args [1].type == TCL_INT) ? (double) args [1].intValue :
-                                                  args [1].doubleValue;
+        resultPtr->type = TCL_WIDE_INT;
+	if (isMax) {
+	    resultPtr->wideValue = (w0 < w1) ? w1 : w0;
+	} else {
+	    resultPtr->wideValue = (w0 > w1) ? w1 : w0;
+	}
+#endif
+    } else {
+	/*
+	 * Compare as ints.
+	 */
+	long i0 = args[0].intValue;
+	long i1 = args[1].intValue;
 
-        if (values [0] < values [1]) {
-            resultPtr->doubleValue = values [0];
-        } else {
-            resultPtr->doubleValue = values [1];
-        }
+        resultPtr->type = TCL_INT;
+	if (isMax) {
+	    resultPtr->intValue = (i0 < i1) ? i1 : i0;
+	} else {
+	    resultPtr->intValue = (i0 > i1) ? i1 : i0;
+	}
     }
     return TCL_OK;
 }
@@ -353,38 +343,20 @@ TclX_MathInit (interp)
     minMaxArgTypes [0] = TCL_EITHER;
     minMaxArgTypes [1] = TCL_EITHER;
 
-    Tcl_CreateObjCommand (interp, 
-			  "max",
-			  TclX_MaxObjCmd,
-                          (ClientData) NULL,
-			  (Tcl_CmdDeleteProc*) NULL);
+    Tcl_CreateObjCommand (interp, "max", TclX_MaxObjCmd,
+	    (ClientData) NULL, (Tcl_CmdDeleteProc*) NULL);
 
-    Tcl_CreateObjCommand (interp,
-			  "min",
-			  TclX_MinObjCmd,
-                          (ClientData) NULL,
-			  (Tcl_CmdDeleteProc*) NULL);
+    Tcl_CreateObjCommand (interp, "min", TclX_MinObjCmd,
+	    (ClientData) NULL, (Tcl_CmdDeleteProc*) NULL);
 
-    Tcl_CreateObjCommand (interp,
-                          "random",
-			  TclX_RandomObjCmd,
-                          (ClientData) NULL,
-			  (Tcl_CmdDeleteProc*) NULL);
+    Tcl_CreateObjCommand (interp, "random", TclX_RandomObjCmd,
+	    (ClientData) NULL, (Tcl_CmdDeleteProc*) NULL);
 
-    Tcl_CreateMathFunc (interp,
-                        "max",
-                        2, 
-			minMaxArgTypes,
-                        TclX_MaxFunc,
-                        (ClientData) NULL);
+    Tcl_CreateMathFunc(interp, "max", 2, minMaxArgTypes,
+	    TclX_MinMaxFunc, (ClientData) 1 /* IS_MAX */);
 
-    Tcl_CreateMathFunc (interp, 
-			"min",
-                        2, 
-			minMaxArgTypes,
-                        TclX_MinFunc,
-                        (ClientData) NULL);
+    Tcl_CreateMathFunc (interp, "min", 2, minMaxArgTypes,
+	    TclX_MinMaxFunc, (ClientData) 0 /* IS_MIN */);
 
 }
-
 
