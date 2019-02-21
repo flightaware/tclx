181dabb3abb930b7a3c0e65386226e0e4ef8dcbf
Karl Lehenbauer <karl@flightaware.com>
2019-02-21 20:22:18 +0000

    declare TclX_ChannelFdInit in include to avoid compiler warning

--- generic/tclExtdInt.h
+++ generic/tclExtdInt.h
@@ -611,6 +611,9 @@ int
 TclXOSSetCloseOnExec (Tcl_Interp *interp,
                       Tcl_Channel channel,
                       int         value);
+
+void
+TclX_ChannelFdInit (Tcl_Interp *interp);
 #endif
 
 /* vim: set ts=4 sw=4 sts=4 et : */
